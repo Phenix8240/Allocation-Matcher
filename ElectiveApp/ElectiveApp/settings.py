7cb4649f51419fb568e751584d302ab495426601
kushal-a11y <kushalstcet666@gmail.com>
2025-05-19 19:45:57 +0000

    env

--- ElectiveApp/ElectiveApp/settings.py
+++ ElectiveApp/ElectiveApp/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5u(2r&930paf5hv^)nyku*n9pb69#ge7q)*e@(dyn%141qgv-a"
-
+SECRET_KEY = os.getenv('SECRET_KEY') 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
