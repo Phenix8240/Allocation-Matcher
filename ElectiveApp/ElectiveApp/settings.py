2cfbe07ecc5c08bfa54f8d5f6f4e01aef61bb02d
kushal-a11y <kushalstcet666@gmail.com>
2025-05-19 21:24:30 +0000

    added render env

--- ElectiveApp/ElectiveApp/settings.py
+++ ElectiveApp/ElectiveApp/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
